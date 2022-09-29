161cc6912e7d0420310c54581e276e3f1dd8a3be
ProfMonkey07 <caldermcqueen@gmail.com>
2022-09-28 20:35:10 -0700

    maybe git will stop bullying me

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("Welcome back!");
+    let ferris_ascii = "
+        _~^~^~_
+    \\) /  o o  \\ (/
+      '_   -   _'
+      / '-----' \\";
+    let mut ferris_hunger: i32 = 0;
+    let mut ferris_boredom: i32 = 0;
+    let mut shells: i32 = 5;
+    let mut age: i32 = 0;
+    println!("{ferris_ascii}");
+}
+
+fn game_loop(hunger: &mut i32, boredom: &mut)
+
+
