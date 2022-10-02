55c7e48a7a893f9e1d70c323a5eaca27a84b198f
ProfMonkey07 <caldermcqueen@gmail.com>
2022-10-01 19:16:24 -0700

    added some json stuff

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 #[allow(unused_imports)]
 use std::io;
+use std::env;
+use std::fs;
 use std::thread;
 use std::time::Duration;
-
+//enum to hold possible ferris emotions
 enum Emotions {
     Happy,
     Sad,
@@ -10,7 +12,7 @@ enum Emotions {
     Bored,
     Sleepy,
 }
-
+//struct to hold ferris and his attributes
 struct Ferris {
     hunger: i32,
     boredom: i32,
@@ -20,6 +22,9 @@ struct Ferris {
 }
 
 fn main() {
+    let json_path = "ferris.json";
+    let ferris_json = fs::read_to_string(json_path).expect("something is wrong with the save");
+    println!("{ferris_json}");
     println!("Welcome back!");
     let mut currentferris = initialise(0, 0, 5, 0.0, "happy");
     let ferris_ascii = "
@@ -34,7 +39,7 @@ fn main() {
         thread::sleep(Duration::from_millis(10));
     }
 }
-
+//this will be used at the beginning to load data from the json save
 fn initialise(hunger: i32, boredom: i32, shells: i32, age: f32, emo: &str) -> Ferris {
     return Ferris {
         hunger,
@@ -51,7 +56,7 @@ fn initialise(hunger: i32, boredom: i32, shells: i32, age: f32, emo: &str) -> Fe
         },
     };
 }
-
+//used to update ferris constantly
 fn update(state: Ferris, hincrease: i32, bincrease: i32, sincrease: i32, aincrease: f32) -> Ferris {
     return Ferris {
         hunger: state.hunger + hincrease,
